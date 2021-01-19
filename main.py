b933d497d72c39126b430e66048c4a87d6bedc72
Vanjiga <vanja.gacic123@gmal.com>
2021-01-19 12:52:15 +0100

    ehh

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ amount = int(input("How many cars would you like to add? "))
 if amount > 0:
     car_type=[]
     for i in range(amount):
-        manufacturer = str(input(""))
-        model = str(input(""))
-        year_of_production = int(input(""))
-        number_of_doors = int(input(""))
+        manufacturer = str(input("manufacturer of the car? "))
+        model = str(input("model of the car? "))
+        year_of_production = int(input("year of production? "))
+        number_of_doors = int(input("number of doors?"))
         power = float(input(""))
         engine_volume = float(input(""))
         fuel_type = str(input(""))
@@ -20,7 +20,13 @@ if amount > 0:
 
     for j in range (amount -1):
         for i in range (amount -1):
-            if car.price
+            if car.price_of_registracion(car_type[i]) > \
+                car.price_of_registracion(car_type[i+1]):
+                temp = car_type[i]
+                car_type[i] = car_type[i+1]
+                car_type[i+1] = temp
+
+    print('\nCar with least amount of registration cost: \n')
 
 
 else:
